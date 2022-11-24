23f988b41dd9768591aa1033fc72d9a69978becd
Mike Hepburn <eformat@gmail.com>
2022-11-24 13:56:04 +1000

    📻 demo url 📻

--- src/main/java/com/acme/stocks/SSESource.java
+++ src/main/java/com/acme/stocks/SSESource.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 public class SSESource extends RichSourceFunction<String> {
 
-    private String url = "https://quote-generator-quote-generator.apps.foo.sandbox1962.opentlc.com/quotes/stream"; //https://flask-stocks-flask-stocks.apps.openshift-411-x24tf.demo.redhatlabs.dev";
+    private String url = "https://quote-generator-quote-generator.apps.foo.sandbox1962.opentlc.com/quotes/stream";
     private final Logger logger;
 
     public SSESource(Logger logger) {
